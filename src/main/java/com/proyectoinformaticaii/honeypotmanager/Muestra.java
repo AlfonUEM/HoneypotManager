014e7fb443e05b36175cfd06f0bb5949a272ff4c
alfon <alfon@alfons-mbp.home>
2023-06-04 12:46:15 +0200

    Quitando comentarios puestos por NetBeans

--- src/main/java/com/proyectoinformaticaii/honeypotmanager/Muestra.java
+++ src/main/java/com/proyectoinformaticaii/honeypotmanager/Muestra.java
@@ -1,15 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.proyectoinformaticaii.honeypotmanager;
 
 import javafx.beans.property.SimpleStringProperty;
 
-/**
- *
- * @author alfon
- */
 public class Muestra {
     
     private int id;
